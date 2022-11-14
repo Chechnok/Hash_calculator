38e7da1ae3347eef3523655e84aababfebb3ea5a
Chechnoken <bolgartim@gmail.com>
2022-11-14 19:38:32 +0200

    classes added

---
+++ Hash_calculator/Classes/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_calculator.Classes
+{
+    internal class Calculator
+    {
+    }
+}
